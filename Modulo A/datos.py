74f5cf57a9290761d097823e78b28a32e6efdf76
Albert González <albertandres08@gmail.com>
2025-12-24 00:27:10 -0400

    Add files via upload

---
+++ Modulo A/datos.py
@@ -0,0 +1,48 @@
+# ==========================================
+# CONFIGURACIÓN GENERAL DEL JUEGO (config.py)
+# ==========================================
+
+# 1. Dimensiones de la Pantalla (Estilo Vertical)
+ancho_ventana = 450
+alto_ventana = 800
+fps = 60
+
+# 2. Colores (Formato RGB)
+color_fondo = (30, 30, 30)      # Gris muy oscuro
+color_agua = (52, 152, 219)     # Azul claro
+color_barco = (127, 140, 141)   # Gris metálico
+color_tocado = (231, 76, 60)    # Rojo
+color_fallo = (236, 240, 241)   # Blanco/Gris claro
+color_texto = (255, 255, 255)   # Blanco
+color_borde = (41, 128, 185)    # Azul oscuro para la grilla
+
+# 3. Configuración del Tablero
+filas = 10
+columnas = 10
+tamaño_celda = 40  # Cada cuadro mide 40x40 píxeles
+margen_x = 25      # Espacio a la izquierda de la grilla
+margen_y = 150     # Espacio arriba para poner títulos/menús
+
+# 4. Estados de las Celdas (Lo que hay en la matriz)
+agua = 0
+barco = 1
+fallo = 2    # Disparaste al agua
+tocado = 3   # Le diste a un barco
+hundido = 4  # El barco ya no existe
+
+# 5. Definición de la Flota (Módulo A: Backend)
+flota = [
+    {"nombre": "Portaaviones", "tamaño": 5},
+    {"nombre": "Acorazado",    "tamaño": 4},
+    {"nombre": "Acorazado",    "tamaño": 4},
+    {"nombre": "Submarino",    "tamaño": 3},
+    {"nombre": "Submarino",    "tamaño": 3},
+    {"nombre": "Submarino",    "tamaño": 3},
+    {"nombre": "Destructor",   "tamaño": 2},
+    {"nombre": "Destructor",   "tamaño": 2},
+    {"nombre": "Destructor",   "tamaño": 2},
+    {"nombre": "Destructor",   "tamaño": 2}
+]
+
+# 6. Rutas de Archivos (Persistencia)
+archivo_historial = "historial.txt"
